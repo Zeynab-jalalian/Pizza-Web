08ef94894b95598fe100d24ee212d8f8b31e699c
Zeynab Jalalian <zjalalianweb@example.com>
2025-03-13 13:00:41 +0330

    header-top-right

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 document.querySelector('.phone-dropdown-icon').addEventListener('click',function(){
     document.querySelector('.phone-container').classList.toggle('open');
 })
+/* personel-account */
+document.querySelector('.account-dropdown-toggle').addEventListener('click',function(){
+    document.querySelector('.personal-account').classList.toggle('open');
+})
