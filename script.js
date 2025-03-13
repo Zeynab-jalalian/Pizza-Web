655c027a96ba40a138fcecc5f449c1de047e081c
Zeynab Jalalian <zjalalianweb@example.com>
2025-03-13 12:43:21 +0330

    header-top-left

---
+++ script.js
@@ -0,0 +1,4 @@
+/* phone-container */
+document.querySelector('.phone-dropdown-icon').addEventListener('click',function(){
+    document.querySelector('.phone-container').classList.toggle('open');
+})
