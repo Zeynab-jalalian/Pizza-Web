8f5df720d4af293a351606fecd8017dd7c2c64cf
Zeynab Jalalian <zjalalianweb@example.com>
2025-03-13 13:19:01 +0330

    develope the js for dropdown of top header

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-/* phone-container */
-document.querySelector('.phone-dropdown-icon').addEventListener('click',function(){
+/* phone-container and personal account */
+document.querySelector('.phone-dropdown-icon').addEventListener('click', function(event) {
+    event.stopPropagation(); 
     document.querySelector('.phone-container').classList.toggle('open');
-})
-/* personel-account */
-document.querySelector('.account-dropdown-toggle').addEventListener('click',function(){
+});
+
+document.querySelector('.account-dropdown-toggle').addEventListener('click', function(event) {
+    event.stopPropagation(); 
     document.querySelector('.personal-account').classList.toggle('open');
-})
+});
+
+document.addEventListener('click', function(event) {
+    
+    if (!event.target.closest('.phone-container') && !event.target.closest('.personal-account')) {
+        document.querySelector('.phone-container').classList.remove('open');
+        document.querySelector('.personal-account').classList.remove('open');
+    }
+});
