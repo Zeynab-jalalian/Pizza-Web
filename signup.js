6461e7d4c0af050d5b67162e4807a91e468ea590
Zeynab Jalalian <zjalalianweb@example.com>
2025-03-24 16:05:27 +0330

    validation signup

--- signup.js
+++ signup.js
@@ -14,7 +14,7 @@ document.getElementById("signup-email").addEventListener("input", function() {
     }
 
     
-    toggleLoginButton();
+    toggleSignupButton();
 });
 //validation password
 document.getElementById("signup-pass").addEventListener("input", function() {
@@ -31,19 +31,44 @@ document.getElementById("signup-pass").addEventListener("input", function() {
     }
 
    
-    toggleLoginButton();
+    toggleSignupButton();
 });
-
-//login status
-function toggleLoginButton() {
+//validation Confirm password
+document.getElementById("signup-pass-confirm").addEventListener("input",function(){
+    let password=document.getElementById("signup-pass").value.trim();
+    let ConfirmPassword=this.value.trim();
+    if(ConfirmPassword===password){
+        this.style.borderBottom = "2px solid green"; 
+    }else{
+        this.style.borderBottom = "2px solid red"; 
+    }
+    if (ConfirmPassword == "") {
+        this.style.borderBottom = "1px solid #000"; 
+    }
+    toggleSignupButton();
+})
+// validation Name
+document.getElementById("signup-name").addEventListener("input",function(){
+    let nameInput=this.value.trim()
+   if(nameInput===""){
+    this.style.borderBottom = "2px solid #000"; 
+   }else{
+    this.style.borderBottom = "2px solid green"; 
+   }
+    toggleSignupButton();
+})
+//Signup status
+function toggleSignupButton() {
+    let name=document.getElementById("signup-name").value.trim();
     const email = document.getElementById("signup-email").value.trim();
     const password = document.getElementById("signup-pass").value.trim();
+    const ConfirmPassword=document.getElementById("signup-pass-confirm").value.trim();
 
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const passwordPattern = /^.{8,}$/;
 
 
-    if (emailPattern.test(email) && passwordPattern.test(password) && email !== "" && password !== "") {
+    if (emailPattern.test(email) && passwordPattern.test(password) && password===ConfirmPassword && email !== "" && password !== "" && ConfirmPassword!=="" && name!=="") {
         document.querySelector(".signup-btn").disabled = false;
         document.querySelector(".signup-btn").style.opacity = "1"; 
         document.querySelector(".signup-btn").style.cursor = "pointer"; 
